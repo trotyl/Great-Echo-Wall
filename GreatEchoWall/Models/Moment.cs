acb1473f9da841f8dd486ab9566ee34db1e24781
trotyl <yzj1995@vip.qq.com>
2015-03-28 23:18:36 +0800

    增加Record和Moment类属性

---
+++ GreatEchoWall/Models/Moment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatEchoWall.Models
+{
+    public class Moment
+    {
+        public DateTime SendStart { get; set; }
+        public DateTime SendEnd { get; set; }
+        public DateTime RecvEnd { get; set; }
+    }
+}
---
+++ GreatEchoWall/Models/Record.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatEchoWall.Models
+{
+    public class Record
+    {
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public string Content { get; set; }
+        public int Times { get; set; }
+        public IPEndPoint LocalEndPoint { get; set; }
+        public IPEndPoint remoteEndPoint { get; set; }
+        public int RouteCount { get; set; }
+        public string RouteLog { get; set; }
+        public DateTime TcpConnectStart { get; set; }
+        public DateTime TcpConnectEnd { get; set; }
+        public DateTime TcpCloseStart { get; set; }
+        public DateTime TcpCloseEnd { get; set; }
+        public List<Moment> TcpMoments { get; set; }
+        public DateTime UdpConnectStart { get; set; }
+        public DateTime UdpConnectEnd { get; set; }
+        public DateTime UdpCloseStart { get; set; }
+        public DateTime UdpCloseEnd { get; set; }
+        public List<Moment> UdpMoments { get; set; }
+    }
+}
